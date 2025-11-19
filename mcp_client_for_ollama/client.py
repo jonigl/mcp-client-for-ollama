9ee7b38c5defe8815066690199479a9bb5ed9487
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2025-11-19 21:04:12 +0100

    fix: adding missing arg for available tools in the follow up chat params

--- mcp_client_for_ollama/client.py
+++ mcp_client_for_ollama/client.py
@@ -358,6 +358,7 @@ class MCPClient:
                 "model": model,
                 "messages": messages,
                 "stream": True,
+                "tools": available_tools,
                 "options": model_options
             }
 
